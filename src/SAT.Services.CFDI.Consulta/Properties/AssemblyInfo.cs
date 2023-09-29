2a3fcf7333a364ee2f25bf11df4af5ae597cb900
Antonio Hernandez <anhedz@gmail.com>
2023-09-28 19:16:47 -0600

    proyecto para pruebas

---
+++ src/SAT.Services.CFDI.Consulta/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada con un ensamblado.
+[assembly: AssemblyTitle("Jaeger.SAT.ConsultaCFDI")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Impostores Profesionales SA de CV")]
+[assembly: AssemblyProduct("Jaeger.SAT.ConsultaCFDI")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("c9af1772-2f04-458e-bbde-922830b76599")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
+// utilizando el carácter "*", como se muestra a continuación:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
---
+++ tester/Jaeger.SAT.Tester/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Jaeger.SAT.CFDI.Consulta.Interfaces;
+using Jaeger.SAT.CFDI.Consulta.Services;
+
+namespace Jaeger.SAT.Tester {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("***************************************************************************************************");
+            Console.WriteLine("** SERVICIO DE CONSULTA ");
+            Console.WriteLine("***************************************************************************************************");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Status service = new Status();
+
+            // Build request
+            IRequest request = CFDI.Consulta.Request.Create()
+                                .WithEmisorRFC("SOC190205N7A")
+                                .WithReceptorRFC("DCM1203237I0")
+                                .WithTotal(10)
+                                .WithFolioFiscal("F9A186DA-BDD4-494C-AA1E-A41C926808AC")
+                                .Build();
+
+            var response = service.Execute(request);
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(response.CodigoEstatus);
+            Console.WriteLine("Respuesta: \r\n" + response.EsCancelable);
+            Console.ReadLine();
+        }
+    }
+}
